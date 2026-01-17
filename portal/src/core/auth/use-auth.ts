eb350d909aecbb3755df3773c0c4c6e6b170d5ba
Paul Garton <paulgarton1984@hotmail.com>
2026-01-17 21:15:17 +0000

    add firebase create user auth function

---
+++ portal/src/core/auth/use-auth.ts
@@ -0,0 +1,17 @@
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const auth = getAuth();
+
+export const createAuthAccount = async (email: string, password: string) => {
+  try {
+    const userRecord = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    return userRecord;
+  } catch (error) {
+    const errorMessage = (error as Error).message;
+    throw new Error(`Error creating user: ${errorMessage}`);
+  }
+};
